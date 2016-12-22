3d01b891937e839989ab55337cc504b009d8764b
Madhvesh Navkal <manavkal@visa.com>
2016-12-21 16:51:23 -0800

    check heroku commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('Hello world, I am a maddy bot')
 })
 
 // for Facebook verification
